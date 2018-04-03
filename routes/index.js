7c3e55f734b7707b8ebb8240edefc788859342d5
jonghunkwon <jonghunkwon@jonghunui-MacBook.local>
2018-04-03 21:11:22 +0900

    v1 router 추가

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const v1 = require("./v1");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+router.use("/v1", v1);
 
 module.exports = router;
